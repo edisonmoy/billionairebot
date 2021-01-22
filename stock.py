91b9f39b49625c12bbb3b5ca05d999bdd7356f85
junj1017 <junj1017@berkeley.edu>
2021-01-22 09:48:00 +0900

    testing

--- stock.py
+++ stock.py
@@ -2,22 +2,35 @@ import websocket, json
 import requests
 import os
 from dotenv import load_dotenv
+import alpaca_trade_api as tradeapi
+from alpaca_trade_api import StreamConn
 
 
 
 load_dotenv()
-
+SOCKET = "wss://data.alpaca.markets/stream"
 AP_KEY = os.getenv("ALPACA_KEY")
 APS_KEY = os.getenv("ALPACA_SECRET_KEY")
+AP_PAPER_URL = "https://paper-api.alpaca.markets"
+
+class Trading:
+    def __init__(self):
+        self.api = tradeapi.REST('<key_id>', '<secret_key>', AP_PAPER_URL)
+    
+    def trade(self):
+        print('hello')
+
+
+#testing streaming data
 
 def open(ws):
     print("opened")
-    auth_data = {
+    ws_stream_conn = {
         "action": "authenticate",
         "data": {"key_id": AP_KEY, "secret_key": APS_KEY}
     }
 
-    ws.send(json.dumps(auth_data))
+    ws.send(json.dumps(ws_stream_conn))
 
     listen_message = {"data": {"streams": ["AM.PLTR"]}}
 
@@ -31,7 +44,10 @@ def response(ws, message):
 def close(ws):
     print("closed")
 
-socket = "wss://data.alpaca.markets/stream"
 
-ws = websocket.WebSocketApp(socket, on_open=open, on_message=response, on_close=close)
-ws.run_forever()
+
+# ws = websocket.WebSocketApp(SOCKET, on_open=open, on_message=response, on_close=close)
+# ws.run_forever()
+
+tradingbot = Trading()
+tradingbot.trade()
